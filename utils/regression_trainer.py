a6c55d661edee813a968250b1b4ff4585ed2e5ae
muhammadmujir <muhammadmujir011@gmail.com>
2022-06-21 06:23:31 +0700

    using original model

--- utils/regression_trainer.py
+++ utils/regression_trainer.py
@@ -11,8 +11,8 @@ import torch.nn as nn
 import logging
 import numpy as np
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-# from  models.vgg import vgg19
-from models.custom_model import CSRNet
+from  models.vgg import vgg19
+# from models.custom_model import CSRNet
 from datasets.crowd import Crowd
 from losses.bay_loss import Bay_Loss
 from losses.post_prob import Post_Prob
@@ -67,8 +67,8 @@ class RegTrainer(Trainer):
                                           num_workers=args.num_workers*self.device_count,
                                           pin_memory=(True if x == 'train' else False))
                             for x in ['train', 'val']}
-        # self.model =vgg19()
-        self.model = CSRNet()
+        self.model = vgg19()
+        # self.model = CSRNet()
         self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
